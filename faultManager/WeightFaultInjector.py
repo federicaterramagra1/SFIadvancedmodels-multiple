6360a5d8808dd54768d9fd6370e59c3b834bf13f
federicaterramagra1 <fterramagra@gmail.com>
2025-03-04 16:20:36 +0000

    multiple faults added

--- faultManager/WeightFaultInjector.py
+++ faultManager/WeightFaultInjector.py
@@ -45,7 +45,7 @@ class WeightFaultInjector:
             for fault in faults:
                 # Access the layer through the `module` attribute of the QuantWrapper
                 fault.layer_name = f'module.{fault.layer_name}'  # Add 'module.' prefix
-                layer = getattr(self.network.module, fault.layer_name)
+                layer = getattr(self.network, fault.layer_name)
                 weight_tensor = layer.weight.data.view(torch.uint8)
                 # Flip the specified bit
                 weight_tensor[fault.tensor_index] = weight_tensor[fault.tensor_index] ^ (1 << fault.bit)
@@ -60,7 +60,7 @@ class WeightFaultInjector:
         with torch.no_grad():
             for fault in faults:
                 fault.layer_name = f'module.{fault.layer_name}'  # Add 'module.' prefix
-                layer = getattr(self.network.module, fault.layer_name)
+                layer = getattr(self.network, fault.layer_name)
                 weight_tensor = layer.weight.data.view(torch.uint8)
                 # Set the bit to the specified value
                 if fault.value == 1:
